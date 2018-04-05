167a9406d0535d2c55650692cd200822690581f3
tehw0lf <robert.weyres@ruhr-uni-bochum.de>
2018-04-06 01:36:55 +0200

    Restructure project
    A second implementation has been added that was used in the first version of paw. It is faster, but not memory efficient.

--- wgen/tests/__init__.py
+++ wgen/tests/__init__.py
@@ -1,14 +1,27 @@
 import filecmp
 import unittest
+import os
 import wgen
 
 
 class wgen_test(unittest.TestCase):
-    def test_3digits(self):
-        cset = {0: '123456789', 1: '123456789', 2: '123456789'}
-        sample = 'wgen/tests/files/sample_3digits.lst'
+    def setUp(self):
+        self.cset = {0: '123', 1: 'ABC', 2: '!"§ '}
+        if os.name == 'nt':
+            self.sample = 'wgen/tests/files/sample_mixed_dos.lst'
+        else:
+            self.sample = 'wgen/tests/files/sample_mixed_unix.lst'
+
+    def test_mixed_words(self):
         tfilepath = 'wgen/tests/files/tmp'
-        with open(tfilepath, 'w') as tfile:
-            for word in wgen.gen_wordlist(cset):
+        with open(tfilepath, 'w', encoding='utf-8') as tfile:
+            for word in wgen.gen_words(self.cset):
                 tfile.write('%s\n' % word)
-        self.assertTrue(filecmp.cmp(tfilepath, sample))
+        self.assertTrue(filecmp.cmp(tfilepath, self.sample))
+
+    def test_mixed_list(self):
+        tfilepath = 'wgen/tests/files/tmp'
+        with open(tfilepath, 'w', encoding='utf-8') as tfile:
+            for word in wgen.gen_words(self.cset):
+                tfile.write('%s\n' % word)
+        self.assertTrue(filecmp.cmp(tfilepath, self.sample))
