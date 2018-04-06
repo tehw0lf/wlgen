0cf55631ab9679673e1f386e2df70f5e9b70c40c
tehw0lf <tehwolf@protonmail.com>
2018-04-06 02:01:25 +0200

    Update PyPI description

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@ from setuptools import setup
 
 setup(name='wgen',
       version='1.0',
-      description='a recursive wordlist generator written in python',
+      description='A recursive wordlist generator written in python',
+      long_description='''A recursive wordlist generator written in Python.
+For each string position, custom character sets can be defined.
+Currently there are two implementations to generate a wordlist.
+gen_wordlist builds the whole list in memory before writing it,
+gen_words is a generator that is memory efficient but slower.
+''',
       url='https://github.com/tehw0lf/wgen',
       author='tehw0lf',
       author_email='tehwolf@protonmail.com',
