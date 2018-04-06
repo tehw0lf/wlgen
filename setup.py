491f72a7b0476739975299f0802bb819de413ca4
tehw0lf <tehwolf@protonmail.com>
2018-04-06 02:10:19 +0200

    Rename project
    Moving from wgen to wlgen.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='wgen',
+setup(name='wlgen',
       version='1.0',
       description='A recursive wordlist generator written in python',
       long_description='''A recursive wordlist generator written in Python.
@@ -9,10 +9,10 @@ Currently there are two implementations to generate a wordlist.
 gen_wordlist builds the whole list in memory before writing it,
 gen_words is a generator that is memory efficient but slower.
 ''',
-      url='https://github.com/tehw0lf/wgen',
+      url='https://github.com/tehw0lf/wlgen',
       author='tehw0lf',
       author_email='tehwolf@protonmail.com',
       license='MIT',
-      packages=['wgen'],
+      packages=['wlgen'],
       zip_safe=False
       )
--- wlgen/tests/__init__.py
+++ wlgen/tests/__init__.py
@@ -1,27 +1,27 @@
 import filecmp
 import unittest
 import os
-import wgen
+import wlgen
 
 
-class wgen_test(unittest.TestCase):
+class wlgen_test(unittest.TestCase):
     def setUp(self):
         self.cset = {0: '123', 1: 'ABC', 2: '!"§ '}
         if os.name == 'nt':
-            self.sample = 'wgen/tests/files/sample_mixed_dos.lst'
+            self.sample = 'wlgen/tests/files/sample_mixed_dos.lst'
         else:
-            self.sample = 'wgen/tests/files/sample_mixed_unix.lst'
+            self.sample = 'wlgen/tests/files/sample_mixed_unix.lst'
 
     def test_mixed_words(self):
-        tfilepath = 'wgen/tests/files/tmp'
+        tfilepath = 'wlgen/tests/files/tmp'
         with open(tfilepath, 'w', encoding='utf-8') as tfile:
-            for word in wgen.gen_words(self.cset):
+            for word in wlgen.gen_words(self.cset):
                 tfile.write('%s\n' % word)
         self.assertTrue(filecmp.cmp(tfilepath, self.sample))
 
     def test_mixed_list(self):
-        tfilepath = 'wgen/tests/files/tmp'
+        tfilepath = 'wlgen/tests/files/tmp'
         with open(tfilepath, 'w', encoding='utf-8') as tfile:
-            for word in wgen.gen_wordlist(self.cset):
+            for word in wlgen.gen_wordlist(self.cset):
                 tfile.write('%s\n' % word)
         self.assertTrue(filecmp.cmp(tfilepath, self.sample))
