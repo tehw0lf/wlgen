93ccc5ef009b19772c6c1578195dce5174ae32f9
tehw0lf <tehwolf@protonmail.com>
2024-12-24 23:05:12 +0100

    Fix tests

--- src/wlgen/tests/__init__.py
+++ src/wlgen/tests/__init__.py
@@ -8,27 +8,31 @@ class wlgen_test(unittest.TestCase):
     def setUp(self):
         self.cset = {0: "123", 1: "ABC", 2: ' !"$'}
         if os.name == "nt":
-            self.sample = "wlgen/tests/files/sample_mixed_dos"
+            self.sample = "src/wlgen/tests/files/sample_mixed_dos"
         else:
-            self.sample = "wlgen/tests/files/sample_mixed_unix"
+            self.sample = "src/wlgen/tests/files/sample_mixed_unix"
 
     def test_mixed_words(self):
-        tfilepath = "wlgen/tests/files/tmp"
+        tfilepath = "src/wlgen/tests/files/tmp"
         with open(tfilepath, "w", encoding="utf-8") as tfile:
             for word in wlgen.gen_words(self.cset):
                 tfile.write("%s\n" % word)
         self.assertTrue(filecmp.cmp(tfilepath, self.sample))
 
     def test_mixed_list(self):
-        tfilepath = "wlgen/tests/files/tmp"
+        tfilepath = "src/wlgen/tests/files/tmp"
         with open(tfilepath, "w", encoding="utf-8") as tfile:
             for word in wlgen.gen_wordlist(self.cset):
                 tfile.write("%s\n" % word)
         self.assertTrue(filecmp.cmp(tfilepath, self.sample))
 
     def test_mixed_iter(self):
-        tfilepath = "wlgen/tests/files/tmp"
+        tfilepath = "src/wlgen/tests/files/tmp"
         with open(tfilepath, "w", encoding="utf-8") as tfile:
             for word in wlgen.gen_wordlist_iter(self.cset):
                 tfile.write("%s\n" % word)
         self.assertTrue(filecmp.cmp(tfilepath, self.sample))
+
+
+if __name__ == "__main__":
+    unittest.main()
