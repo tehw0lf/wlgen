b3ea7f2430b4860b8c2a1165d0c575c1d8e1fd61
tehw0lf <tehwolf@protonmail.com>
2026-06-12 21:21:26 +0200

    Retrieve version dynamically

--- wlgen/__init__.py
+++ wlgen/__init__.py
@@ -4,12 +4,14 @@ This package provides multiple implementations for generating wordlists
 by calculating the n-ary Cartesian product of input character sets.
 """
 
+from importlib.metadata import version
+
 from wlgen.core import (
+    estimate_wordlist_size,
     gen_wordlist,
     gen_wordlist_iter,
     gen_words,
     generate_wordlist,
-    estimate_wordlist_size,
 )
 
 __all__ = [
@@ -19,4 +21,4 @@ __all__ = [
     "generate_wordlist",
     "estimate_wordlist_size",
 ]
-__version__ = "2.0.0"
+__version__ = version("wlgen")
